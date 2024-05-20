7d5a7d2ae4edeb0132f2fb8e0cfb75d9ea662ea8
Nuh Jama <nuh@kth.se>
2024-05-20 16:31:12 +0200

    Initial commit
    Initialize project with basic spring setup files.

---
+++ fakebankapi/src/main/java/com/atlar/fakebankapi/FakebankapiApplication.java
@@ -0,0 +1,13 @@
+package com.atlar.fakebankapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakebankapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakebankapiApplication.class, args);
+	}
+
+}
---
+++ fakebankapi/src/test/java/com/atlar/fakebankapi/FakebankapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atlar.fakebankapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakebankapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
